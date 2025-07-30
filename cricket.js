a90efa365dff8654841351b5de5b984d8d938b9e
jhanzaibkhatri1999 <jhanzeb.khatri1561@gmail.com>
2025-07-30 18:31:50 +0500

    added local storage

--- cricket.js
+++ cricket.js
@@ -1,3 +1,18 @@
+let scoreStr = localStorage.getItem('Score');
+let score;
+resetScore(scoreStr);
+
+function resetScore(scoreStr) {
+  score = scoreStr ? JSON.parse(scoreStr) : {
+    win: 0, loss: 0, Tie: 0,
+  }
+
+  score.displayScore = function () {
+    return `Won: ${score.win}, Loss: ${score.loss}, Tie: ${score.Tie}`;
+  };
+}
+
+
 function compChoice() {
   let randomNumber = Math.random() * 3;
   if (randomNumber > 0 && randomNumber <= 1) {
@@ -11,16 +26,26 @@ function compChoice() {
 
 function getResult(compChoice, playerChoice) {
   if (compChoice === playerChoice) {
+    score.Tie++;
     return 'Match Has Been Drawn';
   } else if ((compChoice === 'Bat' && playerChoice === 'Ball') ||
     (compChoice === 'Ball' && playerChoice === 'Stump') ||
     (compChoice === 'Stump' && playerChoice === 'Bat')) {
+    score.loss++;
     return 'Computer Wins';
   } else {
+    score.win++;
     return 'Player Wins';
   }
 };
 
 function showResult(compChoice, playerChoice, resultMsg) {
-  alert(`You have chosen ${playerChoice}. Computer choice is ${compChoice} and ${resultMsg}`);
+  localStorage.setItem('Score', JSON.stringify(score));
+  alert(`You have chosen ${playerChoice}. Computer choice is ${compChoice} and 
+
+    ${resultMsg}
+    
+    ${score.displayScore()} `);
+
+
 }
